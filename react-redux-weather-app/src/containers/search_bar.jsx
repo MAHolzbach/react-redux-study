20c29c6a4aeb7f97b7a88862d6b2d646f3124e0b
Mike Holzbach <maholzbach@gmail.com>
2017-12-29 15:02:47 -0500

    controlled components and binding

--- react-redux-weather-app/src/containers/search_bar.jsx
+++ react-redux-weather-app/src/containers/search_bar.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formInput: ""
+    };
+  }
+
+  updateInput = event => {
+    this.setState({
+      formInput: event.target.value
+    });
+  };
+
+  clearInput = e => {
+    e.preventDefault();
+    this.setState({ formInput: "" });
+  };
+
   render() {
     return (
-      <form action="" className="input-group">
-        <input type="text" placeholder="Enter city..." />
+      <form action="" className="input-group" onSubmit={this.clearInput}>
+        <input
+          type="text"
+          placeholder="Enter city..."
+          className="form-control"
+          onChange={this.updateInput}
+          value={this.state.formInput}
+        />
         <button type="submit" className="btn btn-primary">
           Search!
         </button>
