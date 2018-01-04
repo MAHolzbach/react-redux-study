cf7d40d4396b8c3f21abd7e23a23b8ae436a0b22
Mike Holzbach <maholzbach@gmail.com>
2018-01-04 17:03:10 -0500

    Incorprate Sparklines
    Create chart component

--- react-redux-weather-app/src/containers/weather_list.jsx
+++ react-redux-weather-app/src/containers/weather_list.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Chart from "../components/chart";
 
 class WeatherList extends Component {
   renderWeather = cityData => {
     const city = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
     return (
       <tr key={city}>
         <td>{city}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
       </tr>
     );
   };
@@ -17,8 +22,8 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Conditions</th>
-            <th>High (F)</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
             <th>Humidity</th>
           </tr>
         </thead>
