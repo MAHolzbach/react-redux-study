e8b31d735015ba3c810cccd4f6a82272537560d8
Mike Holzbach <maholzbach@gmail.com>
2018-01-05 09:17:31 -0500

    Add units and conversion to F. Add conditions

--- react-redux-weather-app/src/components/chart.jsx
+++ react-redux-weather-app/src/components/chart.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Sparklines, SparklinesLine } from "react-sparklines";
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+} from "react-sparklines";
+import sum from "lodash/sum";
+import round from "lodash/round";
+
+const average = data => {
+  return round(sum(data) / data.length);
+};
 
 const Chart = props => {
   return (
     <div>
       <Sparklines height={80} width={100} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>
+        {average(props.data)} {props.units}
+      </div>
     </div>
   );
 };
--- react-redux-weather-app/src/containers/weather_list.jsx
+++ react-redux-weather-app/src/containers/weather_list.jsx
@@ -5,12 +5,20 @@ import Chart from "../components/chart";
 class WeatherList extends Component {
   renderWeather = cityData => {
     const city = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = cityData.list.map(
+      weather => 9 / 5 * (weather.main.temp - 273) + 32
+    );
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const conditions = cityData.list[0].weather[0].main;
     return (
       <tr key={city}>
         <td>{city}</td>
+        <td>{conditions}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="F" />
+        </td>
+        <td>
+          <Chart data={humidity} color="black" units="%" />
         </td>
       </tr>
     );
@@ -22,9 +30,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Current Conditions</th>
+            <th>Temperature (F)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
