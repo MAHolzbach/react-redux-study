0da3940b3aed4b8b0769da301e965e8f57a9582e
Mike Holzbach <maholzbach@gmail.com>
2018-01-13 14:55:43 -0500

    Create form. Implement redux-form.

--- blog-app/src/components/PostsNew.jsx
+++ blog-app/src/components/PostsNew.jsx
@@ -1,7 +1,76 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-export default class PostsNew extends Component {
+class PostsNew extends Component {
+  renderField = field => {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          placeholder={field.placeholder}
+          {...field.input}
+        />
+        {field.meta.error}
+      </div>
+    );
+  };
+
+  onSubmit = values => {
+    console.log(values);
+  };
+
   render() {
-    return <div>PostsNew!</div>;
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field
+          name="title"
+          label="Title"
+          placeholder="Enter title..."
+          component={this.renderField}
+        />
+        <Field
+          name="categories"
+          label="Categories"
+          placeholder="Add tags for searching..."
+          component={this.renderField}
+        />
+        <Field
+          name="content"
+          label="Content"
+          placeholder="Write your post!"
+          component={this.renderField}
+        />
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+        <button className="btn btn-danger">Cancel</button>
+      </form>
+    );
   }
 }
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Please add at least one tag!";
+  }
+
+  if (!values.content) {
+    errors.content = "You gotta write something!";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(PostsNew);
